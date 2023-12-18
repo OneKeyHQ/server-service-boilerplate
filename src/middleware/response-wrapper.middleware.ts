e597f387f6c0f1ab73b37d6c1899c4a1447a7d48
loatheb <zhangzhao@onekey.so>
2023-12-18 17:37:25 +0800

    chore: remove proto files

--- src/middleware/response-wrapper.middleware.ts
+++ src/middleware/response-wrapper.middleware.ts
@@ -1,4 +1,5 @@
 import { IMiddleware, Middleware } from '@midwayjs/core';
+import * as i18nServiceBasic from '@midwayjs/i18n';
 import { Context, NextFunction } from '@midwayjs/koa';
 
 @Middleware()
@@ -6,11 +7,14 @@ export class ResponseWrapperMiddleware
   implements IMiddleware<Context, NextFunction>
 {
   resolve() {
-    return async (_: Context, next: NextFunction) => {
+    return async (ctx: Context, next: NextFunction) => {
       const result = await next();
+      const i18nService = await ctx.requestContext.getAsync(
+        i18nServiceBasic.MidwayI18nService
+      );
       return {
         code: 0,
-        message: 'success',
+        message: i18nService.translate('msg__success'),
         data: result,
       };
     };
